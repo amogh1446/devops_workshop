6a38cd737a3e5f2012857c4370d30d41a609e831
amogh1446 <1si24cs011@sit.ac.in>
2025-12-03 04:07:26 +0530

    try again

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("changes have been made");
+        System.out.println("jenkins activities");
 
 }
 }
